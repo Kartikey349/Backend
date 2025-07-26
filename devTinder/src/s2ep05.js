439bb1ea65216265bad554ba61d972c0eabeab23
kartikey sharma <kartikey7518@gmail.com>
2025-07-27 02:43:53 +0530

    error handling

--- devTinder/src/s2ep05.js
+++ devTinder/src/s2ep05.js
@@ -12,47 +12,67 @@ const app = express();
 // })
 
 
-app.use("/admin", (req,res, next) => {
-    console.log("credentials being checked");
-    const token = "xyzaxy";
-    const is_authorised = token === "xyz";
-    if(!is_authorised){
-        res.status(401).send("unauthorised request")
-    }else{
-        next();
+// app.use("/admin", (req,res, next) => {
+//     console.log("credentials being checked");
+//     const token = "xyzaxy";
+//     const is_authorised = token === "xyz";
+//     if(!is_authorised){
+//         res.status(401).send("unauthorised request")
+//     }else{
+//         next();
+//     }
+// })
+
+
+// app.get("/admin/getData", (req,res) => {
+//     res.send("All data sent");
+// })
+
+// app.get("/admin/deleteData", (req,res) => {
+//     res.send("deleted all data");
+// })
+
+
+// //for login we dont need authorisation
+// app.post("/user/login" , (req, res) => {
+//     res.send("recieved login details")
+// })
+
+// app.use("/user", (req, res,next) => {
+//     console.log("checking user credentials");
+//     const token = "xyz";
+//     const authorisedUser = token === "xyz";
+//     if(!authorisedUser){
+//         res.status(401).send("authorised user")
+//     }else{
+//         next();
+//     }
+// })
+
+// app.get("/user/data", (req, res) => {
+//     res.send("all user data");
+// })
+
+
+app.get("/user", (req, res) => {
+    // try{
+    //     throw new Error("adhadadadkja");
+    //     res.send("user data sent")
+    // }catch(err){
+    //     res.status(500).send("something went wrong, contact support team")
+    // }
+
+    throw new Error("adhadadadkja");
+        res.send("user data sent")
+})
+
+
+//always put this at the end, so that it can read the errors occured above
+app.use("/", (err, req, res,next) => {
+    if(err){
+        res.status(500).send("something went wrong")
     }
 })
-
-
-app.get("/admin/getData", (req,res) => {
-    res.send("All data sent");
-})
-
-app.get("/admin/deleteData", (req,res) => {
-    res.send("deleted all data");
-})
-
-
-//for login we dont need authorisation
-app.post("/user/login" , (req, res) => {
-    res.send("recieved login details")
-})
-
-app.use("/user", (req, res,next) => {
-    console.log("checking user credentials");
-    const token = "xyz";
-    const authorisedUser = token === "xyz";
-    if(!authorisedUser){
-        res.status(401).send("authorised user")
-    }else{
-        next();
-    }
-})
-
-app.get("/user/data", (req, res) => {
-    res.send("all user data");
-})
-
 
 app.listen(7777, () => {
     console.log("server is successfully running on port 7777")

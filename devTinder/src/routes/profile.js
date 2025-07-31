885a3b8d77b18cd7ac4bf3221fae380eec87ab3e
kartikey sharma <kartikey7518@gmail.com>
2025-07-31 19:32:05 +0530

    api for password change

--- devTinder/src/routes/profile.js
+++ devTinder/src/routes/profile.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const profileRouter = express.Router();
+const bcrypt = require("bcrypt")
 
 const {userAuth} = require("../middleware/auth");
 const User = require("../models/user");
 const {validateUpdate} = require("../utils/validation");
+const validator = require("validator") 
 
 //use of jwt and cookies---- view profile api
 profileRouter.get("/profile/view", userAuth , async(req, res) => {
@@ -38,6 +40,31 @@ profileRouter.patch("/profile/edit",userAuth, async(req, res) => {
     }catch(err){
         res.status(403).send("Error: " + err.message)
     }
+})
+
+profileRouter.patch("/profile/password",userAuth, async(req, res) => {
+    const currentPassword = req.body.password;
+    const newPassword = req.body.newPassword;
+
+    const user = req.user;
+     try{
+        const isValid = await bcrypt.compare(currentPassword, user.password);
+
+        if(isValid){
+            if (!validator.isStrongPassword(newPassword)) {
+                throw new Error("New password must be stronger");
+            }
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            user.password = hashedPassword;
+            await user.save();
+            res.send("successfully upated")
+        }else{
+            throw new Error("enter right current password");
+        }
+        
+     }catch(err){
+        res.status(500).send("ERROR: "+ err.message)
+     }
 })
 
 module.exports = profileRouter

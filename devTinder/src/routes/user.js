e6447eba2ba03785f52d15275708afab19fe3f2d
kartikey sharma <kartikey7518@gmail.com>
2025-08-01 03:42:27 +0530

    created view connections by loggedin user

--- devTinder/src/routes/user.js
+++ devTinder/src/routes/user.js
@@ -26,4 +26,38 @@ userRouter.get("/user/request/received", userAuth, async(req,res) =>{
 })
 
 
+//get user connection
+userRouter.get("/user/connection", userAuth, async(req,res) => {
+    const loggedInUser = req.user;
+    try{
+        const connections = await ConnectionRequest.find(
+            {
+                $or: [
+                    {
+                        toUserId: loggedInUser._id, status: "accepted"
+                    }, {
+                        fromUserId: loggedInUser._id, status: "accepted"
+                    }
+                ]
+            }
+        ).populate("fromUserId", "firstName lastName skills about age").populate("toUserId", "firstName lastName skills about age")
+
+        const data = connections.map((row) => {
+            if(row.fromUserId.equals(loggedInUser._id)){
+                    return row.toUserId
+            }else{
+                return row.fromUserId
+            }
+        })
+
+        res.json({
+            message: "Here are the connectios",
+            data
+        })
+    }catch(error){
+        res.status(400).send("ERROR: "+ err.message)
+    }  
+})
+
+
 module.exports = userRouter;

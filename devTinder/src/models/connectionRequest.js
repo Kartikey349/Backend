0b1793648403792cc2e5bf34d0c3c9712aac40de
kartikey sharma <kartikey7518@gmail.com>
2025-07-31 19:46:26 +0530

    created connection request api

---
+++ devTinder/src/models/connectionRequest.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["ignored", "interested", "accepted", "rejected"],
+            message: `{VALUE} is incorrect status type`
+        },
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+//compound index --- make query faster
+connectionRequestSchema.index({fromUserId: 1, toUserId: 1});
+
+const ConnectionRequestModel = mongoose.model("ConnectionRequest", connectionRequestSchema);
+
+module.exports = ConnectionRequestModel;
--- devTinder/src/routes/connection.js
+++ devTinder/src/routes/connection.js
@@ -2,6 +2,8 @@ const express = require("express")
 const connectionRouter = express.Router();
 
 const {userAuth} = require("../middleware/auth")
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 //api to send connection request
 connectionRouter.post("/sendConnectionRequest",userAuth, async (req,res) => {
@@ -10,4 +12,64 @@ connectionRouter.post("/sendConnectionRequest",userAuth, async (req,res) => {
     res.send("connection request sent by " + user.firstName)
 })
 
+
+connectionRouter.post("/request/send/:status/:userId", userAuth, async (req, res) => {
+
+    const fromUserId = req.user._id;
+    const toUserId = req.params.userId;
+    const status = req.params.status;
+
+    const allowedStatus = ["ignored", "interested"];
+    try{
+        //validating status type
+        const isValidStatus = allowedStatus.includes(status);
+        if(!isValidStatus){
+            throw new Error("invalid status type: "+ status)
+        }
+
+        //.equals is used bcoz both are objId
+        if(fromUserId.equals(toUserId)){
+            return res.send("you can't send request to yourself")
+        }
+
+        //validate toUserId actually exists in Db
+        const toUser = await User.findById(toUserId);
+        if(!toUser){
+            throw new Error("User not found")
+        }
+        
+
+        //check whether existing connection request
+        const validate = await ConnectionRequest.findOne({
+            $or: [
+                {
+                    toUserId,
+                    fromUserId
+                },{
+                    toUserId: fromUserId,
+                    fromUserId: toUserId
+                }
+            ]})
+
+        if(validate){
+            throw new Error("request already exists")
+        }
+
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        })
+
+        const data = await connectionRequest.save();
+
+        res.json({
+            message: `${req.user.firstName} ${status} ${toUser.firstName}`,
+            data,
+        })
+    }catch(err){
+        res.status(400).send("ERROR: "+ err.message);
+    }
+})
 module.exports = connectionRouter;
